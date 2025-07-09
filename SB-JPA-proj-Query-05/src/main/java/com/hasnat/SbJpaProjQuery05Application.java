67ab23673fa55191a83df24114ea5acd76cf9620
Hasnat alam <hasnatalamofficial@gmail.com>
2025-07-09 23:44:08 +0530

    initial commit

---
+++ SB-JPA-proj-Query-05/src/main/java/com/hasnat/SbJpaProjQuery05Application.java
@@ -0,0 +1,13 @@
+package com.hasnat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJpaProjQuery05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJpaProjQuery05Application.class, args);
+	}
+
+}
