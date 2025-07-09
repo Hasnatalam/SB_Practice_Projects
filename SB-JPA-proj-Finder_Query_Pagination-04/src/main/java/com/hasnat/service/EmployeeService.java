f425adf55fe414db938f8ba3bbea87c1a43bd5d3
Hasnat alam <hasnatalamofficial@gmail.com>
2025-07-09 23:03:03 +0530

    Finder method implementation

--- SB-JPA-proj-Finder_Query_Pagination-04/src/main/java/com/hasnat/service/EmployeeService.java
+++ SB-JPA-proj-Finder_Query_Pagination-04/src/main/java/com/hasnat/service/EmployeeService.java
@@ -3,9 +3,33 @@ package com.hasnat.service;
 import java.util.List;
 
 import com.hasnat.dto.EmployeeResponse;
+import com.hasnat.entity.Employee;
 
 public interface EmployeeService {
 	public EmployeeResponse getEmployee(Long id);
 	public List<EmployeeResponse> getAllEmployees();
+	
+	
+	public List<EmployeeResponse> getAllEmployeesByRole(String role);
+	
+	public List<EmployeeResponse> getAllEmployeesByNameAndRole(String name, String role);
+	
+	public List<EmployeeResponse> getAllEmployeesByNameOrEmail(String name, String email);
+	
+	public List<EmployeeResponse> getAllEmployeesBySalaryBetween(Double minSalary, Double maxSalary);
+	
+	public List<EmployeeResponse> getAllEmployeesBySalaryLessThan(Double salary);
+	
+	public List<EmployeeResponse> getAllEmployeesBySalaryGreaterThan(Double salary);
+	
+	public List<EmployeeResponse> getAllEmployeesByRoleIn(List<String> role);
+	
+	public Long countByRole(String role);
+	
+	public Boolean existsByName(String name);
+	
+	public List<EmployeeResponse> findByNameIsNull();
+	
+	public List<EmployeeResponse> findByNameIsNotNull();
 	
 }
--- SB-JPA-proj-Finder_Query_Pagination-04/src/main/java/com/hasnat/service/EmployeeServiceImp.java
+++ SB-JPA-proj-Finder_Query_Pagination-04/src/main/java/com/hasnat/service/EmployeeServiceImp.java
@@ -2,6 +2,7 @@ package com.hasnat.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -35,7 +36,131 @@ public class EmployeeServiceImp implements EmployeeService{
 
 	@Override
 	public List<EmployeeResponse> getAllEmployees() {
-		return null;
+		
+		List<Employee> employees = employeeRepository.findAll();
+		
+		List<EmployeeResponse> employeeResponses = employees.stream().map(emp-> EmployeeResponse.builder()
+				.id(emp.getId())
+				.name(emp.getName())
+				.role(emp.getRole())
+				.email(emp.getEmail())
+				.salary(emp.getSalary())
+				.build()).collect(Collectors.toList());
+		
+		return employeeResponses;
 	}
+
+	@Override
+	public List<EmployeeResponse> getAllEmployeesByRole(String role) {
+		List<Employee> employee = employeeRepository.findAllByRole(role);
+		return employee.stream().map(emp-> EmployeeResponse.builder()
+				.id(emp.getId())
+				.name(emp.getName())
+				.role(emp.getRole())
+				.email(emp.getEmail())
+				.salary(emp.getSalary()).build()).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<EmployeeResponse> getAllEmployeesByNameAndRole(String name, String role) {
+		List<Employee> employee = employeeRepository.findAllByNameAndRole(name, role);
+		
+		return employee.stream().map(emp-> EmployeeResponse.builder()
+				.id(emp.getId())
+				.name(emp.getName())
+				.role(emp.getRole())
+				.email(emp.getEmail())
+				.salary(emp.getSalary()).build()).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<EmployeeResponse> getAllEmployeesByNameOrEmail(String name, String email) {
+		
+		List<Employee> employee = employeeRepository.findAllByNameOrEmail(name, email);
+		
+		return employee.stream().map(emp-> EmployeeResponse.builder()
+				.id(emp.getId())
+				.name(emp.getName())
+				.role(emp.getRole())
+				.email(emp.getEmail())
+				.salary(emp.getSalary()).build()).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<EmployeeResponse> getAllEmployeesBySalaryBetween(Double minSalary, Double maxSalary) {
+		List<Employee> employee = employeeRepository.findAllBySalaryBetween(minSalary, maxSalary);
+		return employee.stream().map(emp-> EmployeeResponse.builder()
+				.id(emp.getId())
+				.name(emp.getName())
+				.role(emp.getRole())
+				.email(emp.getEmail())
+				.salary(emp.getSalary()).build()).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<EmployeeResponse> getAllEmployeesBySalaryLessThan(Double salary) {
+		List<Employee> employee = employeeRepository.findAllBySalaryLessThan(salary);
+		return employee.stream().map(emp-> EmployeeResponse.builder()
+				.id(emp.getId())
+				.name(emp.getName())
+				.role(emp.getRole())
+				.email(emp.getEmail())
+				.salary(emp.getSalary()).build()).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<EmployeeResponse> getAllEmployeesBySalaryGreaterThan(Double salary) {
+		List<Employee> employee = employeeRepository.findAllBySalaryGreaterThan(salary);
+		return employee.stream().map(emp-> EmployeeResponse.builder()
+				.id(emp.getId())
+				.name(emp.getName())
+				.role(emp.getRole())
+				.email(emp.getEmail())
+				.salary(emp.getSalary()).build()).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<EmployeeResponse> getAllEmployeesByRoleIn(List<String> role) {
+		List<Employee> employee = employeeRepository.findAllByRoleIn(role);
+		return employee.stream().map(emp-> EmployeeResponse.builder()
+				.id(emp.getId())
+				.name(emp.getName())
+				.role(emp.getRole())
+				.email(emp.getEmail())
+				.salary(emp.getSalary()).build()).collect(Collectors.toList());
+	}
+
+	@Override
+	public Long countByRole(String role) {
+		return employeeRepository.countByRole(role);
+	}
+
+	@Override
+	public Boolean existsByName(String name) {
+		return employeeRepository.existsByName(name);
+	}
+
+	@Override
+	public List<EmployeeResponse> findByNameIsNull() {
+		List<Employee> employee = employeeRepository.findByNameIsNull();
+		return employee.stream().map(emp-> EmployeeResponse.builder()
+				.id(emp.getId())
+				.name(emp.getName())
+				.role(emp.getRole())
+				.email(emp.getEmail())
+				.salary(emp.getSalary()).build()).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<EmployeeResponse> findByNameIsNotNull() {
+		List<Employee> employee = employeeRepository.findByNameIsNotNull();
+		return employee.stream().map(emp-> EmployeeResponse.builder()
+				.id(emp.getId())
+				.name(emp.getName())
+				.role(emp.getRole())
+				.email(emp.getEmail())
+				.salary(emp.getSalary()).build()).collect(Collectors.toList());
+	}
+	
 	
 }
