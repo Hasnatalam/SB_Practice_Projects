72ba6530b82b4454965804a32f689febab691212
Hasnat alam <hasnatalamofficial@gmail.com>
2025-07-09 23:30:10 +0530

    Initial coomit

---
+++ SB-JPA-proj-Finder-04/src/main/java/com/hasnat/SbJpaProjFinder04Application.java
@@ -0,0 +1,13 @@
+package com.hasnat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJpaProjFinder04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJpaProjFinder04Application.class, args);
+	}
+
+}
